32c5948cfb75a75ba80260c1a02af701cda10744
Peter Brommer <pbro@itap.phyik.uni-stuttgart.de>
2003-02-04 16:00:39 +0000

    Improved sloppy programming to avoid FPE in log calculation

--- force.c
+++ force.c
@@ -64,7 +64,10 @@ real calc_forces_pair(real *xi, real *forces)
       x1=x0+pair_pot.step[col];
       y0=xi[first];
       y1=xi[first+1];
-      grad = y0*log(y0/y1) / (x0*log(x0/x1));
+      if (y0*y1>0)
+	  grad=(y0*log(y0/y1)) /  (x0*log(x0/x1));
+      else
+	  grad=1e30;
       if (!((grad>-1e10) && (grad<1e10))) grad=1e30;
       if (  (grad>-1e-20)&& (grad<1e-20)) grad=0.;
       spline_ed(pair_pot.step[col], xi+first,
