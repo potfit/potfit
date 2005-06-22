d091ccb98c43f040d40b0620912413cdad18f9d5
Peter Brommer <pbro@itap.phyik.uni-stuttgart.de>
2005-06-22 14:23:50 +0000

    Corrected 2 bugs in brent.c, one of them fatal to convergence, re-indentation make changes look larger than they are. The fatal bug would reuse old values of q, p, r. The minor bug concerns sign of w_lower and an incorrect < in check of validity of parabolic interpolation.

--- brent_r.c
+++ brent_r.c
@@ -29,8 +29,8 @@
 *   Boston, MA  02110-1301  USA
 */
 /****************************************************************
-* $Revision: 1.7 $
-* $Date: 2005/05/06 13:24:16 $
+* $Revision: 1.8 $
+* $Date: 2005/06/22 14:23:50 $
 *****************************************************************/
 
 /**** rewritten for double precision                                      ****
@@ -98,7 +98,7 @@ real brent_r(real ax, real bx, real cx, real fbx, real tol,
   for (iter=1;iter<=ITMAX;iter++) {
     midpoint = 0.5 * (x_left + x_right);
     t2=2*(tolerance=(tol*fabs(z)+ZEPS));
-    w_lower = (z - x_left);
+    w_lower = ( x_left - z);
     w_upper = (x_right - z);
     if (fabs(z-midpoint) <= t2-0.5*(x_right-x_left)) {
       *xmin=z;
@@ -114,7 +114,7 @@ real brent_r(real ax, real bx, real cx, real fbx, real tol,
     if (fabs (e) > tolerance)
     {
       /* fit parabola */
-
+      
       r = (z - w) * (f_z - f_v);
       q = (z - v) * (f_z - f_w);
       p = (z - v) * q - (z - w) * r;
@@ -131,52 +131,57 @@ real brent_r(real ax, real bx, real cx, real fbx, real tol,
 
       r = e;
       e = d;
-    }
 
-  if (fabs (p) < fabs (0.5 * q * r) && p < q * w_lower && p < q * w_upper)
-    {
-
-      d = p / q;
-      u = z + d;
-
-      if ((u - x_left) < t2 || (x_right - u) < t2)
+      if (fabs (p) < fabs (0.5 * q * r) && p > q * w_lower && p < q * w_upper)
+      {
+	
+	d = p / q;
+	u = z + d;
+	
+	if ((u - x_left) < t2 || (x_right - u) < t2)
         {
           d = (z < midpoint) ? tolerance : -tolerance ;
         }
+      }
+      else
+      {
+	e = (z < midpoint) ? x_right - z : -(z - x_left) ;
+	d = CGOLD * e;
+      }
     }
-  else
+    else 
     {
       e = (z < midpoint) ? x_right - z : -(z - x_left) ;
       d = CGOLD * e;
     }
 
 
-  if (fabs (d) >= tolerance)
+    if (fabs (d) >= tolerance)
     {
       u = z + d;
     }
-  else
+    else
     {
       u = z + ((d > 0) ? tolerance : -tolerance) ;
     }
-   
-
-  for (j=0;j<ndimtot;j++) vecu[j]=xicom[j]+u*delcom[j];/*set vecu */
-  f_u=(*calc_forces)(vecu,p_u,0); 
-
-  if (f_u > f_z)
+    
+    
+    for (j=0;j<ndimtot;j++) vecu[j]=xicom[j]+u*delcom[j];/*set vecu */
+    f_u=(*calc_forces)(vecu,p_u,0); 
+    
+    if (f_u > f_z)
     {
       if (u < z)
-        {
-          x_left = u;
-	  /* fertig */
-        }
+      {
+	x_left = u;
+	/* fertig */
+      }
       else
-        {
-          x_right = u;
-	  /* done */
-        }
-    
+      {
+	x_right = u;
+	/* done */
+      }
+      
       if (f_u <= f_w || w == z)
       {
 	v = w;
@@ -188,32 +193,32 @@ real brent_r(real ax, real bx, real cx, real fbx, real tol,
       }
       else if (f_u <= f_v || v == z || v == w)
       {
-      v = u;
-      f_v = f_u;
-      /* done */
+	v = u;
+	f_v = f_u;
+	/* done */
       }
     }
-  else if (f_u <= f_z)
-  {
-    if (u < z)
+    else if (f_u <= f_z)
     {
-      x_right = z;
+      if (u < z)
+      {
+	x_right = z;
+      }
+      else
+      {
+	x_left = z;
+      }
+      SHIFT(v,w,z,u);
+      SHIFT(f_v,f_w,f_z,f_u);
+      P_SWAP(p_w,p_z,p_temp);
+      P_SWAP(p_z,p_u,p_temp);
+      /* done */
     }
     else
     {
-      x_left = z;
+      error("Problems in Brent minimization");
     }
-    SHIFT(v,w,z,u);
-    SHIFT(f_v,f_w,f_z,f_u);
-    P_SWAP(p_w,p_z,p_temp);
-    P_SWAP(p_z,p_u,p_temp);
-    /* done */
-  }
-  else
-  {
-    error("Problems in Brent minimization");
-  }
-  
+    
   }
   error("Too many iterations in Brent minimization");
 }
