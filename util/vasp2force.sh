dc180ad809464a249f12baa905cb84de1a0dd32e
Peter Brommer <pbro@itap.phyik.uni-stuttgart.de>
2005-07-04 08:48:19 +0000

    Now using better energy value and safe grepping for marks left by vasp makers.

--- util/vasp2force.sh
+++ util/vasp2force.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #/****************************************************************
-#* $Revision: 1.2 $
-#* $Date: 2004/03/23 10:07:00 $
+#* $Revision: 1.3 $
+#* $Date: 2005/07/04 08:48:19 $
 #*****************************************************************/
 
 [ -f ../single_atom_energies ] || { echo file ../single_atom_energies not found; exit;}
@@ -23,8 +23,9 @@ cat OUTCAR | awk '
     print boxy_v[1]*scale " " boxy_v[2]*scale " " boxy_v[3]*scale; 
     print boxz_v[1]*scale " " boxz_v[2]*scale " " boxz_v[3]*scale; 
   }
-  ($2=="ENERGIE") {getline; getline; getline; getline;
-     print ($4-single_energy)/a[ntypes] }
+# Marker for energy is two spaces between energy and without.
+# Correct energy is energy(sigma->0)
+  /energy  without/ {printf("%.10f\n",($7-single_energy)/a[ntypes]) }
   ($2=="kB") {print $3/1602., $4/1602., $5/1602., $6/1602., $7/1602., $8/1602.}
   ($2=="TOTAL-FORCE") { getline; getline;
      for (i=1; i<=a[ntypes]; i++) { print b[i],$1,$2,$3,$4,$5,$6; getline; } 
