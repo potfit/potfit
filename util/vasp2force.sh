3990dac7eb2a9f43254c22f405be68402cad888f
Peter Brommer <pbro@itap.phyik.uni-stuttgart.de>
2006-05-11 07:27:39 +0000

    Force files from ab-initio MD trajectory

--- util/vasp2force.sh
+++ util/vasp2force.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 #/****************************************************************
-#* $Revision: 1.3 $
-#* $Date: 2005/07/04 08:48:19 $
+#* $Revision: 1.4 $
+#* $Date: 2006/05/11 07:27:39 $
 #*****************************************************************/
 
 [ -f ../single_atom_energies ] || { echo file ../single_atom_energies not found; exit;}
-cat OUTCAR | awk '
-  BEGIN { getline saeng < "../single_atom_energies"; 
+wdir=`pwd`
+cat OUTCAR | awk -v wdir="${wdir}" '  BEGIN { 
+    getline saeng < "../single_atom_energies"; 
     getline < "POSCAR"; getline scale < "POSCAR";
     getline boxx < "POSCAR"; getline boxy < "POSCAR"; getline boxz < "POSCAR";
     getline < "POSCAR"; ntypes = split($0,a);single_energy=0.;
@@ -18,15 +19,34 @@ cat OUTCAR | awk '
     split(boxx,boxx_v);
     split(boxy,boxy_v);
     split(boxz,boxz_v);
-    print a[ntypes]; 
-    print boxx_v[1]*scale " " boxx_v[2]*scale " " boxx_v[3]*scale; 
-    print boxy_v[1]*scale " " boxy_v[2]*scale " " boxy_v[3]*scale; 
-    print boxz_v[1]*scale " " boxz_v[2]*scale " " boxz_v[3]*scale; 
-  }
+  };
 # Marker for energy is two spaces between energy and without.
 # Correct energy is energy(sigma->0)
-  /energy  without/ {printf("%.10f\n",($7-single_energy)/a[ntypes]) }
-  ($2=="kB") {print $3/1602., $4/1602., $5/1602., $6/1602., $7/1602., $8/1602.}
-  ($2=="TOTAL-FORCE") { getline; getline;
+  /energy  without/ {
+    energy=($7-single_energy)/a[ntypes];
+    delete stress;
+  }
+# Find box vectors
+  /VOLUME and BASIS/ {
+    for (i=1;i<=4;i++) getline;
+    getline boxx; getline boxy; getline boxz;
+    split(boxx,boxx_v);
+    split(boxy,boxy_v);
+    split(boxz,boxz_v);
+}
+  ($2=="kB") { 
+     for (i=1;i<=6;i++) stress[i]=$(i+2)/1602.;
+}
+  ($2=="TOTAL-FORCE") { 
+     print "#N",a[ntypes],1; #flag indicates whether to use forces or not
+     print "## force file generated from directory " wdir;
+     print "#X",boxx_v[1]*scale " " boxx_v[2]*scale " " boxx_v[3]*scale; 
+     print "#Y",boxy_v[1]*scale " " boxy_v[2]*scale " " boxy_v[3]*scale; 
+     print "#Z",boxz_v[1]*scale " " boxz_v[2]*scale " " boxz_v[3]*scale; 
+     printf("#E %.10f\n",energy) ;
+     if ( 1 in stress ) 
+       print "#S",stress[1],stress[2],stress[3],stress[4],stress[5],stress[6];
+     print "#F";  
+     getline; getline;
      for (i=1; i<=a[ntypes]; i++) { print b[i],$1,$2,$3,$4,$5,$6; getline; } 
-  }'
+  };' 
