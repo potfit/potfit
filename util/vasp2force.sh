92303430802525c05446514be70413db20a638a5
Peter Brommer <pbro@itap.phyik.uni-stuttgart.de>
2008-10-09 09:11:29 +0000

    Copyrighted

--- util/vasp2force.sh
+++ util/vasp2force.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
+####################################################################
+# 
+#   Copyright 2003--2008 Peter Brommer
+#             Institute for Theoretical and Applied Physics
+#             University of Stuttgart, D-70550 Stuttgart, Germany
+#             http://www.itap.physik.uni-stuttgart.de/
+#
+####################################################################
+#   
+#   This file is part of potfit.
+#
+#   potfit is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   potfit is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with potfit; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin St, Fifth Floor, 
+#   Boston, MA  02110-1301  USA
+# 
 #/****************************************************************
-#* $Revision: 1.5 $
-#* $Date: 2008/08/14 11:25:53 $
+#* $Revision: 1.6 $
+#* $Date: 2008/10/09 09:11:29 $
 #*****************************************************************/
 
 [ -f ../single_atom_energies ] || { echo file ../single_atom_energies not found; exit;}
