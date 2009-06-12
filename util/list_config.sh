46bb5a587cf0c060ffb59c900ceec42ed43c2078
Daniel Schopf <daniel@itap.physik.uni-stuttgart.de>
2009-06-12 09:10:40 +0000

    add header and cvs tags for list_config.sh update Makefile as well

--- util/list_config.sh
+++ util/list_config.sh
@@ -1,4 +1,35 @@
-#!/bin/bash
+#!/bin/sh
+####################################################################
+#
+#   Copyright 2009 Daniel Schopf
+#             Institute for Theoretical and Applied Physics
+#             University of Stuttgart, D-70550 Stuttgart, Germany
+#             http://www.itap.physik.uni-stuttgart.de/
+#
+####################################################################
+#
+#   This file is part of potfit.
+#
+#   potfit is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   potfit is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with potfit; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin St, Fifth Floor,
+#   Boston, MA  02110-1301  USA
+#
+#/****************************************************************
+#* $Revision: 1.2 $
+#* $Date: 2009/06/12 09:10:40 $
+#*****************************************************************/
+
 if [ $# -eq 0 ]; then
 	echo "Usage: get_config FILENAME"
 	exit
