potfit/potfit

commit c97482cc9c85b46bf72010a901459bf540073585

fix compiler warnings from latest GCC update

src/potential_input_f0.c
@@ -322,7 +322,6 @@ void read_chemical_potentials(apot_state* pstate)
   apot_table_t* apt = &g_pot.apot_table;
 
   char buffer[255];
-  char name[255] = {0};
 
   fpos_t filepos;
 
@@ -362,17 +361,8 @@ void read_chemical_potentials(apot_state* pstate)
                      &apt->pmin[i][j], &apt->pmax[i][j]))
         error(1, "Could not read chemical potential for %d. atomtype.\n", j);
 
-      /* split cp and _# */
-      char* token = strchr(buffer, '_');
-
-      if (token != NULL) {
-        strncpy(name, buffer, strlen(buffer) - strlen(token));
-        name[strlen(buffer) - strlen(token)] = '\0';
-      }
-
-      if (strcmp("cp", name) != 0) {
-        if (strlen(name))
-          fprintf(stderr, "Found \"%s\" instead of \"cp\"\n", name);
+      if (strncmp("cp_", buffer, 3) != 0) {
+        error(0, "Found \"%s\" instead of \"cp_<element>\"\n", buffer);
         error(1, "No chemical potentials found in %s.\n", pstate->filename);
       }
 
@@ -757,11 +747,9 @@ void read_analytic_potentials(apot_state* pstate)
 
     // split name and _sc
     char* token = strrchr(name, '_');
-    if (token != NULL && strcmp(token + 1, "sc") == 0) {
-      strncpy(buffer, name, strlen(name) - 3);
-      buffer[strlen(name) - 3] = '\0';
-      strcpy(name, buffer);
+    if (token != NULL && strcmp(token, "_sc") == 0) {
       g_pot.smooth_pot[i] = 1;
+      name[strlen(name) - 3] = '\0';
     }
 
     // check if potential is "pohlong" and change it to bjs
@@ -775,7 +763,7 @@ void read_analytic_potentials(apot_state* pstate)
             pstate->filename, name);
 
     apt->names[i] = (char*)Malloc((strlen(name) + 1) * sizeof(char));
-    strncpy(apt->names[i], name, strlen(name) + 1);
+    sprintf(apt->names[i], "%s", name);
     apt->n_par[i] = apot_get_num_parameters(name);
 
     // add one parameter for cutoff function if _sc is found
@@ -851,23 +839,20 @@ void read_analytic_potentials(apot_state* pstate)
                            &apt->pmin[i][j], &apt->pmax[i][j]);
 
       if (strlen(buffer))
-        strncpy(apt->param_name[i][j], buffer, 30);
+        sprintf(apt->param_name[i][j], "%s", buffer);
 
       /* if last char of name is "!" we have a global parameter */
       if (strrchr(apt->param_name[i][j], '!') != NULL) {
-        apt->param_name[i][j][strlen(apt->param_name[i][j]) - 1] = '\0';
         int l = -1;
         for (int k = 0; k < apt->globals; k++) {
-          if (strcmp(apt->param_name[i][j],
-                     apt->param_name[g_pot.global_pot][k]) == 0)
+          if (strncmp(apt->param_name[i][j],
+            apt->param_name[g_pot.global_pot][k], strlen(apt->param_name[i][j])) == 0)
             l = k;
         }
 
         if (-1 == l)
-          error(1, "Could not find global parameter %s!\n",
+          error(1, "Could not find global parameter %s\n",
                 apt->param_name[i][j]);
-
-        sprintf(apt->param_name[i][j], "%s!", apt->param_name[i][j]);
 
         /* write index array for global parameters */
         apt->n_glob[l]++;

src/config.c
@@ -916,13 +916,17 @@ void read_sphere_center(char const* pline, config_state* cstate)
 void read_chemical_elements(char* psrc, config_state* cstate)
 {
   int i = 0;
-  char const* pchar = strtok(psrc + 3, " \t\r\n");
+  const char* pchar = strtok(psrc + 3, " \t\r\n");
+  char buffer[5];
 
   if (!cstate->num_fixed_elements) {
     while (pchar != NULL && i < g_param.ntypes) {
-      int len = max(strlen(pchar), 4);
-      strncpy((char*)g_config.elements[i], pchar, len);
-      *((char*)g_config.elements[i] + len) = '\0';
+      if (strlen(pchar) > 4) {
+        memset(buffer, 0, sizeof(buffer));
+        strncpy(buffer, pchar, sizeof(buffer) - 1);
+        pchar = buffer;
+      }
+      sprintf((char*)g_config.elements[i], "%s", pchar);
       pchar = strtok(NULL, " \t\r\n");
       i++;
       cstate->num_fixed_elements++;
@@ -930,10 +934,13 @@ void read_chemical_elements(char* psrc, config_state* cstate)
   } else {
     while (pchar != NULL && i < g_param.ntypes) {
       if (strcmp(pchar, g_config.elements[i]) != 0) {
-        if (atoi(g_config.elements[i]) == i && i > cstate->num_fixed_elements) {
-          int len = max(strlen(pchar), 4);
-          strncpy((char*)g_config.elements[i], pchar, len);
-          *((char*)g_config.elements[i] + len) = '\0';
+        if (atoi(g_config.elements[i]) == i && i >= cstate->num_fixed_elements) {
+          if (strlen(pchar) > 4) {
+            memset(buffer, 0, sizeof(buffer));
+            strncpy(buffer, pchar, sizeof(buffer) - 1);
+            pchar = buffer;
+          }
+          sprintf((char*)g_config.elements[i], "%s", pchar);
           cstate->num_fixed_elements++;
         } else {
           error(0, "Mismatch found in configuration %d, line %d.\n",

src/errors.c
@@ -55,27 +55,6 @@ void write_errors(double* force, double tot)
   // final error report
   printf("\n###### error report ######\n");
 
-  FILE* outfile;
-  char file[255];
-
-  if (g_param.write_output_files) {
-    strcpy(file, g_files.output_prefix);
-    strcat(file, ".error");
-    outfile = fopen(file, "w");
-    if (NULL == outfile)
-      error(1, "Could not open file %s\n", file);
-#if !defined(STRESS)
-    fprintf(outfile, "total error sum %f, count %d (%d forces, %d energies)\n",
-            tot, g_calc.mdim, 3 * g_config.natoms, g_config.nconf);
-#else
-    fprintf(
-        outfile,
-        "total error sum %f, count %d (%d forces, %d energies, %d stresses)\n",
-        tot, g_calc.mdim, 3 * g_config.natoms, g_config.nconf,
-        6 * g_config.nconf);
-#endif  // !STRESS
-  }
-
 #if !defined(STRESS)
   printf("total error sum %f, count %d (%d forces, %d energies)\n", tot,
          g_calc.mdim, 3 * g_config.natoms, g_config.nconf);
@@ -148,6 +127,23 @@ void write_errors(double* force, double tot)
 #endif  // STRESS
 
   if (g_param.write_output_files) {
+    char file[255];
+    sprintf(file, "%s.error", g_files.output_prefix);
+
+    FILE* outfile = fopen(file, "w");
+    if (NULL == outfile)
+      error(1, "Could not open file %s\n", file);
+#if !defined(STRESS)
+    fprintf(outfile, "total error sum %f, count %d (%d forces, %d energies)\n",
+            tot, g_calc.mdim, 3 * g_config.natoms, g_config.nconf);
+#else
+    fprintf(
+        outfile,
+        "total error sum %f, count %d (%d forces, %d energies, %d stresses)\n",
+        tot, g_calc.mdim, 3 * g_config.natoms, g_config.nconf,
+        6 * g_config.nconf);
+#endif  // !STRESS
+
     fprintf(outfile, "sum of force-errors  = %e\t\t( %.3f%% - av: %f)\n", f_sum,
             f_sum / tot * 100, f_sum / (3 * g_config.natoms));
     if (g_param.eweight != 0)
@@ -180,6 +176,7 @@ void write_errors(double* force, double tot)
             "\tforce [meV/A]\tenergy [meV]\tstress [MPa]\terror sum\n");
     fprintf(outfile, "RMS:\t%e\t%e\t%e\t%f\n", rms[0] * 1000, rms[1] * 1000,
             rms[2] / 160.2 * 1000, tot);
+    fclose(outfile);
   }
 
   printf("sum of force-errors  = %e\t\t( %.3f%% - av: %e)\n", f_sum,
@@ -210,8 +207,6 @@ void write_errors(double* force, double tot)
   if (g_param.sweight != 0)
     printf("stress \t%e\t(%f MPa)\n", rms[2], rms[2] / 160.2 * 1000);
 #endif  // STRESS
-  if (g_param.write_output_files)
-    fclose(outfile);
 }
 
 /****************************************************************

src/force_eam_elstat.c
@@ -730,8 +730,8 @@ double calc_forces(double* xi_opt, double* forces, int flag)
 
         /* F O U R T H  loop: calculate monopole-dipole and dipole-dipole forces
          */
-        double rp_i, rp_j, pp_ij, tmp_1, tmp_2;
-        double grad_1, grad_2, srval, srgrad, srval_tail, srgrad_tail,
+        double rp_i = 0, rp_j = 0, pp_ij, tmp_1, tmp_2;
+        double grad_1, grad_2, srval = 0, srgrad = 0, srval_tail, srgrad_tail,
             fnval_sum, grad_sum;
 
         for (i = 0; i < g_config.inconf[h]; i++) { /* atoms */

src/force_elstat.c
@@ -600,8 +600,8 @@ double calc_forces(double* xi_opt, double* forces, int flag)
 
         /* F O U R T H  loop: calculate monopole-dipole and dipole-dipole forces
          */
-        double rp_i, rp_j, pp_ij, tmp_1, tmp_2;
-        double grad_1, grad_2, srval, srgrad, srval_tail, srgrad_tail,
+        double rp_i = 0, rp_j = 0, pp_ij, tmp_1, tmp_2;
+        double grad_1, grad_2, srval = 0, srgrad = 0, srval_tail, srgrad_tail,
             fnval_sum, grad_sum;
         for (i = 0; i < g_config.inconf[h]; i++) { /* atoms */
           atom =

src/functions.c
@@ -93,8 +93,7 @@ void add_potential(const char* name, int npar, fvalue_pointer function)
       function_table.fvalue, (k + 1) * sizeof(fvalue_pointer));
 
   // assign values
-  strncpy(function_table.name[k], name, strlen(name));
-  function_table.name[k][strlen(name)] = '\0';
+  sprintf(function_table.name[k], "%s", name);
   function_table.num_params[k] = npar;
   function_table.fvalue[k] = function;
 

src/memory.c
@@ -216,9 +216,8 @@ void init_interaction_name(const char* name)
 {
   int len = strlen(name);
   char* temp = (char*)Malloc((len + 1) * sizeof(char));
-  strncpy(temp, name, len);
-  temp[len] = '\0';
-  g_pot.interaction_name = (const char*)temp;
+  sprintf(temp, "%s", name);
+  g_pot.interaction_name = temp;
 }
 
 /****************************************************************

src/potential_input.c
@@ -351,6 +351,7 @@ void read_pot_line_G(char* pbuf, potential_state* pstate)
 void read_pot_line_C(char* pbuf, potential_state* pstate)
 {
   char names[g_param.ntypes][5];
+  char buffer[5];
 
   // check if there are enough items
   if (pstate->have_format) {
@@ -359,9 +360,12 @@ void read_pot_line_C(char* pbuf, potential_state* pstate)
       if (str == NULL)
         error(1, "Not enough items in #C header line in file %s.\n",
               pstate->filename);
-      int len = max(strlen(str), 4);
-      strncpy(names[i], str, len);
-      names[i][len] = '\0';
+      if (strlen(str) > 4) {
+        memset(buffer, 0, sizeof(buffer));
+        strncpy(buffer, str, 4);
+        str = buffer;
+      }
+      sprintf(names[i], "%s", str);
     }
   } else
     error(1, "#C needs to be specified after #F in file %s\n", pstate->filename);
@@ -369,8 +373,7 @@ void read_pot_line_C(char* pbuf, potential_state* pstate)
   g_config.elements = (char const**)Malloc(g_param.ntypes * sizeof(char*));
   for (int i = 0; i < g_param.ntypes; i++) {
     g_config.elements[i] = (char*)Malloc((strlen(names[i]) + 1) * sizeof(char));
-    strncpy((char*)g_config.elements[i], names[i], strlen(names[i]));
-    *((char*)g_config.elements[i] + strlen(names[i])) = '\0';
+    sprintf((char*)g_config.elements[i], "%s", names[i]);
   }
 }
 

commit d3381559497d453e254cd8cdbba88987c888f7bb

fix bug with global parameters introduced in c97482c

src/potential_input_f0.c
@@ -846,7 +846,7 @@ void read_analytic_potentials(apot_state* pstate)
         int l = -1;
         for (int k = 0; k < apt->globals; k++) {
           if (strncmp(apt->param_name[i][j],
-            apt->param_name[g_pot.global_pot][k], strlen(apt->param_name[i][j])) == 0)
+            apt->param_name[g_pot.global_pot][k], strlen(apt->param_name[i][j]) - 1) == 0)
             l = k;
         }
 